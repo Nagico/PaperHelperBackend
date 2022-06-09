d7d8d8bdc098eb1b6cb02d4f897e13b4eb688176
NagisaCo <yjr888@vip.qq.com>
2022-06-10 00:29:52 +0800

    feat(entity): 添加note

---
+++ PaperHelper.Entities/Entities/Note.cs
@@ -0,0 +1,20 @@
+﻿namespace PaperHelper.Entities.Entities;
+
+public class Note
+{
+    public int Id { get; set; }
+    
+    
+    public int Type { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    
+    public int ProjectId { get; set; }
+    public virtual Project? Project { get; set; }
+    
+    public int PaperId { get; set; }
+    public virtual Paper? Paper { get; set; }
+    
+    public DateTime CreateTime { get; set; }
+    public DateTime? UpdateTime { get; set; }
+}
---
+++ PaperHelper.Entities/EntityConfigs/NoteConfig.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PaperHelper.Entities.Entities;
+
+namespace PaperHelper.Entities.EntityConfigs;
+
+public class NoteConfig : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> builder)
+    {
+        builder.ToTable("tb_note");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+        
+        builder.Property(x => x.Type)
+            .HasColumnName("type");
+
+        builder.Property(x => x.Title)
+            .HasColumnName("title")
+            .IsRequired();
+
+        builder.Property(x => x.Content)
+            .HasColumnName("content")
+            .IsRequired();
+        
+        builder.Property(x => x.ProjectId)
+            .HasColumnName("project_id");
+        
+        builder.Property(x => x.PaperId)
+            .HasColumnName("paper_id");
+
+        builder.Property(x => x.CreateTime)
+            .HasColumnName("create_time")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.UpdateTime)
+            .HasColumnName("update_time")
+            .ValueGeneratedOnAddOrUpdate();
+        
+        builder.HasOne(x => x.Project)
+            .WithMany(x => x.Notes)
+            .HasForeignKey(x => x.ProjectId);
+        
+        builder.HasOne(x => x.Paper)
+            .WithMany(x => x.Notes)
+            .HasForeignKey(x => x.PaperId);
+
+    }
+}
