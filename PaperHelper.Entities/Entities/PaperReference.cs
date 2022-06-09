808bfce20f7ebc009115cfeb6d22b09ebe34a0e0
NagisaCo <yjr888@vip.qq.com>
2022-06-10 00:30:18 +0800

    feat(entity): 添加reference

---
+++ PaperHelper.Entities/Entities/PaperReference.cs
@@ -0,0 +1,12 @@
+﻿namespace PaperHelper.Entities.Entities;
+
+public class PaperReference
+{
+    public int Id { get; set; }
+    
+    public int PaperId { get; set; }
+    public virtual Paper? Paper { get; set; }
+    
+    public int RefPaperId { get; set; }
+    public virtual Paper? RefPaper { get; set; }
+}
---
+++ PaperHelper.Entities/EntityConfigs/PaperReferenceConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PaperHelper.Entities.Entities;
+namespace PaperHelper.Entities.EntityConfigs;
+
+public class PaperReferenceConfig : IEntityTypeConfiguration<PaperReference>
+{
+    public void Configure(EntityTypeBuilder<PaperReference> builder)
+    {
+        builder.ToTable("tb_paper_reference");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+        
+        builder.Property(x=>x.PaperId)
+            .HasColumnName("paper_id");
+        
+        builder.Property(x=>x.RefPaperId)
+            .HasColumnName("ref_paper_id");
+        
+        builder.HasOne(x=>x.Paper)
+            .WithMany(x => x.ReferenceFrom)
+            .HasForeignKey(x => x.PaperId);
+        
+        builder.HasOne(x=>x.RefPaper)
+            .WithMany(x=>x.References)
+            .HasForeignKey(x=>x.RefPaperId);
+    }
+}
