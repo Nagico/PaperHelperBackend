2592671b8b3e936ed8bb7c4913bbe724d19dd6ce
NagisaCo <yjr888@vip.qq.com>
2022-06-10 00:29:44 +0800

    feat(entity): 添加attachment

---
+++ PaperHelper.Entities/Entities/Attachment.cs
@@ -0,0 +1,20 @@
+﻿namespace PaperHelper.Entities.Entities;
+
+public class Attachment
+{
+    public int Id { get; set; }
+    
+    public int Type { get; set; }
+    public string? Name { get; set; }
+    public string? Ext { get; set; }
+    public Uri? Url { get; set; }
+    
+    public int ProjectId { get; set; }
+    public virtual Project? Project { get; set; }
+    
+    public int PaperId { get; set; }
+    public virtual Paper? Paper { get; set; }
+    
+    public DateTime CreateTime { get; set; }
+    public DateTime? UpdateTime { get; set; }
+}
---
+++ PaperHelper.Entities/EntityConfigs/AttachmentConfig.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PaperHelper.Entities.Entities;
+
+namespace PaperHelper.Entities.EntityConfigs;
+
+public class AttachmentConfig : IEntityTypeConfiguration<Attachment>
+{
+    public void Configure(EntityTypeBuilder<Attachment> builder)
+    {
+        builder.ToTable("tb_attachment");
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Type)
+            .HasColumnName("type");
+        
+        builder.Property(x => x.Name)
+            .HasColumnName("name")
+            .IsRequired();
+
+        builder.Property(x => x.Ext)
+            .HasColumnName("ext");
+
+        builder.Property(x => x.Url)
+            .HasColumnName("url");
+        
+        builder.Property(x => x.ProjectId)
+            .HasColumnName("project_id");
+        
+        builder.Property(x => x.PaperId)
+            .HasColumnName("paper_id");
+
+        builder.Property(x => x.CreateTime)
+            .HasColumnName("create_time")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.UpdateTime)
+            .HasColumnName("update_time")
+            .ValueGeneratedOnAddOrUpdate();
+        
+        builder.HasOne(x => x.Project)
+            .WithMany(x => x.Attachments)
+            .HasForeignKey(x => x.ProjectId);
+        
+        builder.HasOne(x => x.Paper)
+            .WithMany(x => x.Attachments)
+            .HasForeignKey(x => x.PaperId);
+    }
+}
