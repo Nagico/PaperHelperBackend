679f53900e9004a1649115401bddbe4ec75a44f3
NagisaCo <yjr888@vip.qq.com>
2022-06-10 00:30:00 +0800

    feat(entity): 添加paper

---
+++ PaperHelper.Entities/Entities/Paper.cs
@@ -0,0 +1,30 @@
+﻿namespace PaperHelper.Entities.Entities;
+
+public class Paper
+{
+    public int Id { get; set; }
+    
+    public string? Title { get; set; }
+    public string? Abstract { get; set; }
+    public string? Keywords { get; set; }
+    public string? Authors { get; set; }
+    public string? Publication { get; set; }
+    public string? Volume { get; set; }
+    public string? Pages { get; set; }
+    
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public int Day { get; set; }
+    
+    public Uri? Url { get; set; }
+    public string? Doi { get; set; }
+    
+    public List<Attachment>? Attachments { get; set; }
+    public List<Note>? Notes { get; set; }
+    public List<PaperTag>? Tags { get; set; }
+    public List<PaperReference>? References { get; set; }
+    public List<PaperReference>? ReferenceFrom { get; set; }
+
+    public DateTime? CreateTime { get; set; }
+    public DateTime? UpdateTime { get; set; }
+}
---
+++ PaperHelper.Entities/EntityConfigs/PaperConfig.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PaperHelper.Entities.Entities;
+
+namespace PaperHelper.Entities.EntityConfigs;
+
+public class PaperConfig : IEntityTypeConfiguration<Paper>
+{
+    public void Configure(EntityTypeBuilder<Paper> builder)
+    {
+        builder.ToTable("tb_paper");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Title)
+            .HasColumnName("title")
+            .IsRequired();
+
+        builder.Property(x => x.Abstract)
+            .HasColumnName("abstract");
+
+        builder.Property(x => x.Keywords)
+            .HasColumnName("keywords")
+            .IsRequired()
+            .HasDefaultValue("[]");
+
+        builder.Property(x => x.Authors)
+            .HasColumnName("authors")
+            .IsRequired()
+            .HasDefaultValue("[]");
+
+        builder.Property(x => x.Publication)
+            .HasColumnName("publication");
+        
+        builder.Property(x=>x.Volume)
+            .HasColumnName("volume");
+        
+        builder.Property(x=>x.Pages)
+            .HasColumnName("pages");
+
+        builder.Property(x => x.Year)
+            .HasColumnName("year");
+
+        builder.Property(x=>x.Month)
+            .HasColumnName("month");
+        
+        builder.Property(x=>x.Day)
+            .HasColumnName("day");
+        
+        builder.Property(x=>x.Url)
+            .HasColumnName("url");
+        
+        builder.Property(x=>x.Doi)
+            .HasColumnName("doi");
+        
+        builder.Property(x => x.CreateTime)
+            .HasColumnName("create_time")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.UpdateTime)
+            .HasColumnName("update_time")
+            .ValueGeneratedOnAddOrUpdate();
+        
+    }
+}
