6aac08f180eaa04ba2a04e1815665eeb38eddeb6
NagisaCo <yjr888@vip.qq.com>
2022-06-19 11:05:46 +0800

    fix(auth): 密码加密存储

---
+++ PaperHelper/Utils/EncryptionUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PaperHelper.Utils;
+
+public static class EncryptionUtil
+{
+    private static string Md5(string source)
+    {
+        using var md5 = MD5.Create();
+        var strResult = BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(source)));
+        return strResult.Replace("-", "");
+    }
+    
+    public static string Encrypt(string text, string salt)
+    {
+        return Md5(Md5(text) + salt);
+    }
+}
