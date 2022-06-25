31f6eb99d27c5223032dcc2ef3c134f674f92a3a
NagisaCo <yjr888@vip.qq.com>
2022-06-25 15:55:44 +0800

    feat(cors): 添加前端域名

--- PaperHelper/Program.cs
+++ PaperHelper/Program.cs
@@ -33,7 +33,8 @@ builder.Services.AddCors(option =>
                 "http://localhost:8080",
                 "http://localhost:3000", 
                 "http://127.0.0.1:8080",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "https://paperhelper.nagico.cn"
             )
         )
     );
